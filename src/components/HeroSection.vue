803b75b8835d697c5eb7431ef52c4595080a64ac
olaszm <martin1olasz@gmail.com>
2020-11-18 17:47:44 +0000

    moving header images to src

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -4,7 +4,7 @@
       <img
         v-for="i in images"
         :key="i"
-        :src="`./${i}`"
+        :src="require(`@/assets/${i}`)"
         alt="i"
         :class="currentImage == i ? 'show' : ''"
       />
