91b01dd63825c1336ad0749cfcd311fa50fdf290
Martin Olasz <martin1olasz@gmail.com>
2024-06-08 23:17:11 +0100

    404 page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
-    <Header />
-    <div @click="closeCart">
-      <MobileMenu />
-      <router-view />
-      <Footer />
+    <div id="app" v-if="!changeMe">
+        <Header />
+        <div @click="closeCart">
+            <MobileMenu />
+            <router-view />
+            <Footer />
+        </div>
+        <Cart />
+        <PostcodePopUp />
+        <vue-progress-bar></vue-progress-bar>
     </div>
-    <Cart />
-    <PostcodePopUp />
-    <vue-progress-bar></vue-progress-bar>
-  </div>
+    <div id="app" v-else>
+        <NotFound />
+    </div>
 </template>
 
 <script>
@@ -18,54 +21,60 @@ import MobileMenu from "@/components/MobileMenu";
 import PostcodePopUp from "@/components/PostcodePopUp";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import NotFound from "./views/404.vue"
 import { mapState, mapActions } from "vuex";
 export default {
-  components: { Header, Footer, Cart, MobileMenu, PostcodePopUp },
-  created() {
-    this.$Progress.start();
-    let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    document.cookie = `TimeZone=${timeZone}; secure;`;
-    console.log(process.env.VUE_APP_URL);
-
-    this.$router.beforeEach((to, from, next) => {
-      if (to.name != "pricing") {
+    components: { Header, Footer, Cart, MobileMenu, PostcodePopUp, NotFound },
+    data() {
+        return {
+            changeMe: true
+        }
+    },
+    created() {
         this.$Progress.start();
-      }
-      next();
-    });
+        let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        document.cookie = `TimeZone=${timeZone}; secure;`;
+        console.log(process.env.VUE_APP_URL);
 
-    this.$router.afterEach((to) => {
-      if (to.name != "pricing") {
+        this.$router.beforeEach((to, from, next) => {
+            if (to.name != "pricing") {
+                this.$Progress.start();
+            }
+            next();
+        });
+
+        this.$router.afterEach((to) => {
+            if (to.name != "pricing") {
+                this.$Progress.finish();
+            }
+        });
+    },
+    methods: {
+        ...mapActions(["toggleCart", "toggleMenu", "loadFromsessionStorage"]),
+        closeCart() {
+            if (this.isCartOpen) {
+                this.toggleCart();
+            }
+        },
+        closeMenu() {
+            if (this.isMenuOpen) {
+                this.toggleMenu();
+            }
+        },
+    },
+    computed: {
+        ...mapState(["isCartOpen", "isMenuOpen"]),
+    },
+    mounted() {
         this.$Progress.finish();
-      }
-    });
-  },
-  methods: {
-    ...mapActions(["toggleCart", "toggleMenu", "loadFromsessionStorage"]),
-    closeCart() {
-      if (this.isCartOpen) {
-        this.toggleCart();
-      }
-    },
-    closeMenu() {
-      if (this.isMenuOpen) {
-        this.toggleMenu();
-      }
-    },
-  },
-  computed: {
-    ...mapState(["isCartOpen", "isMenuOpen"]),
-  },
-  mounted() {
-    this.$Progress.finish();
-    document.addEventListener("click", (e) => {
-      if (!e.target.className.includes("mobile-menu") && this.isMenuOpen) {
-        this.toggleMenu();
-      }
-    });
+        document.addEventListener("click", (e) => {
+            if (!e.target.className.includes("mobile-menu") && this.isMenuOpen) {
+                this.toggleMenu();
+            }
+        });
 
-    this.loadFromsessionStorage();
-  },
+        this.loadFromsessionStorage();
+    },
 };
 </script>
 
@@ -73,8 +82,8 @@ export default {
 @import "@/styles/main.scss";
 
 .router-link-exact-active {
-  outline: none;
-  color: $primary;
-  font-weight: 400;
+    outline: none;
+    color: $primary;
+    font-weight: 400;
 }
 </style>
